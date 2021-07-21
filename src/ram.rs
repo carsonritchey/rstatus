57a2a456b59c87cdc68ba4587dee2f462d09a08a
Carson Ritchey <ritcheycarson@gmail.com>
2021-07-21 10:59:10 -0500

    fixed typo

--- src/ram.rs
+++ src/ram.rs
@@ -12,7 +12,7 @@ pub fn percent_used() -> u16 {
     let total: String = lines.nth(TOTAL_INDEX).expect("no line at total index").unwrap().chars().filter(|c| c.is_digit(10)).collect();
     let total: u64 = total.parse::<>().unwrap();
 
-    let available: String = lines.nth(AVAILABLE_INDEX).expect("no line at total index").unwrap().chars().filter(|c| c.is_digit(10)).collect();
+    let available: String = lines.nth(AVAILABLE_INDEX).expect("no line at available index").unwrap().chars().filter(|c| c.is_digit(10)).collect();
     let available: u64 = available.parse::<>().unwrap();
 
     return 100 - (available as f32 / total as f32 * 100 as f32 + 0.5) as u16;
